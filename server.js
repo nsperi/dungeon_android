f83dea9172ae0ded2c0180b5ee7fdab379bc1558
Natalia Peripolli <nsperipolli@gmail.com>
2024-05-30 18:59:10 -0300

    f

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 import morgan from "morgan";
 import { engine } from "express-handlebars";
 import path from "path"
 
 import indexRouter from "./src/router/index.router.js";
+import socketCb from "./src/router/index.socket.js";
 import errorHandler from "./src/middlewares/errorHandler.mid.js";
 import pathHander from "./src/middlewares/pathHandler.mid.js";
 import __dirname from "./utils.js";
@@ -11,8 +14,10 @@ import __dirname from "./utils.js";
 const server = express();
 const port = 8080;
 const ready = () => console.log("server ready on port " + port);
-
+const nodeServer = createServer(server)
 server.listen(port, ready);
+const socketServer = new Server(nodeServer)
+socketServer.on("connection", socketCb);
 
 server.engine("handlebars",engine())
 server.set('view engine', 'handlebars')
